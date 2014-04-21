dfd115b4a8e08950e8f2c338731c9397411bef9a
Diego Concha <dapc85@gmail.com>
2014-04-21 01:05:06 -0400

    Added sample code for making calls and sending sms messages

--- main.py
+++ main.py
@@ -17,4 +17,16 @@ title = a.get_text()
 # Create object for Twilio
 
 
-# Schedule call with Twilio
+# Make call with Twilio
+from twilio.rest import TwilioRestClient
+ 
+account_sid = raw_input("Account SID:")
+auth_token = raw_input("Auth Token:")
+target = raw_input("Target (eg. +13334445555):")
+
+client = TwilioRestClient(account_sid, auth_token)
+
+call = client.calls.create(to=target,
+                           from_="+14157234236",
+                           url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
+print call.sid
--- text.py
+++ text.py
@@ -1,13 +1,18 @@
-import twilio
+from twilio.rest import TwilioRestClient
+
+# Get SID and AuthToken
+account_sid = raw_input("Account SID:")
+auth_token = raw_input("Auth Token:")
 
 # Send text
 try:
-    client = twilio.rest.TwilioRestClient(account_sid, auth_token)
- 
-    message = client.sms.messages.create(
+    client = TwilioRestClient(account_sid, auth_token)
+    target = raw_input("Target (eg. +13334445555):")
+    message = client.messages.create(
         body="Hey there sexy ;)",
-        to="+13057209243",
-        from_=""
+        to=target,
+        from_="+14157234236"
     )
+#    print message.sid
 except twilio.TwilioRestException as e:
     print e
